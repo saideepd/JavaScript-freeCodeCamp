4922631d32ff5966cacbc8516699a500ce66c211
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2018-11-08 21:58:16 +0530

    Update new.js

--- new.js
+++ new.js
@@ -452,3 +452,38 @@ console.log(arr);
 
 console.log(arr.splice(2, 2, "JS NUggets"));
 console.log(arr);
+
+
+/* Copying Arrays */
+
+var original = [true, true, undefined, false, null];
+
+// slice
+var copy1 = original.slice(0);
+
+// spread operator
+var copy2 = [...original];
+console.log(copy1, copy2);
+
+/* Copying Arrays */
+
+var original = [true, true, undefined, false, null];
+
+// slice
+var copy1 = original.slice(0);
+
+// spread operator
+var copy2 = [...original];
+console.log(copy1, copy2);
+
+// DEEP Copying
+var deepArray = [["freeCodeCamp"]];
+var shallowCopy = deepArray.slice(0);
+
+// shallowCopy[0].push("is great");
+// console.log(deepArray[0], shallowCopy[0]);
+
+var deepCopy = JSON.parse(JSON.stringify(deepArray));
+
+deepCopy[0].push("is great");
+console.log(deepArray[0], deepCopy[0]);
